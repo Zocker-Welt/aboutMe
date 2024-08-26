2d0c0a32ca5d230968c715201378003a81e33279
Zocker-Welt <166701868+Zocker-Welt@users.noreply.github.com>
2024-08-26 10:24:33 +0300

    Update script.js
    dark mode

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-document.addEventListener("mousedown", function(event) {
-    var rectangle = document.getElementById("myRectangle");
+window.addEventListener("DOMContentLoaded", () => {
+    const checkbox = document.getElementById("checkbox");
 
-    if (event.target === rectangle) {
-        // Calculate the new dimensions
-        var originalWidth = rectangle.offsetWidth;
-        var originalHeight = rectangle.offsetHeight;
-        var newWidth = originalWidth * 1.05; // Increase width by 5%
-        var newHeight = originalHeight * 1.05; // Increase height by 5%
+    checkbox.addEventListener("change", () => {
+        // change theme
+        document.body.classList.toggle("dark");
 
-        // Update the rectangle's dimensions
-        rectangle.style.width = newWidth + "px";
-        rectangle.style.height = newHeight + "px";
-    }
-});
+        const label = document.querySelector(".label");
+        const ball = document.querySelector(".ball");
+        const logo = document.querySelector(".logo-container h1");
+        const p = document.querySelector("body p");
+        const text_container1 = document.querySelector(".text-container1");
+        const text_container1p = document.querySelector(".text-container1 p");
+        const text_container2 = document.querySelector(".text-container2");
+        const text_container2p = document.querySelector(".text-container2 p");
+        const text_container3 = document.querySelector(".text-container3");
+        const text_container3p = document.querySelector(".text-container3 p");
+        const text_container4 = document.querySelector(".text-container4");
+        const text_container4p = document.querySelector(".text-container4 p");
+        const text_container5 = document.querySelector(".text-container5");
+        const text_container5p = document.querySelector(".text-container5 p");
 
-document.addEventListener("mouseup", function(event) {
-    var rectangle = document.getElementById("myRectangle");
-
-    if (event.target === rectangle) {
-        // Calculate the new dimensions
-        var originalWidth = rectangle.offsetWidth;
-        var originalHeight = rectangle.offsetHeight;
-        var newWidth = originalWidth / 1.05; // Restore original width
-        var newHeight = originalHeight / 1.05; // Restore original height
-
-        // Update the rectangle's dimensions
-        rectangle.style.width = newWidth + "px";
-        rectangle.style.height = newHeight + "px";
-    }
+        label.classList.toggle("dark");
+        ball.classList.toggle("dark");
+        logo.classList.toggle("dark");
+        p.classList.toggle("dark");
+        text_container1.classList.toggle("dark");
+        text_container1p.classList.toggle("dark");
+        text_container2.classList.toggle("dark");
+        text_container2p.classList.toggle("dark");
+        text_container3.classList.toggle("dark");
+        text_container3p.classList.toggle("dark");
+        text_container4.classList.toggle("dark");
+        text_container4p.classList.toggle("dark");
+        text_container5.classList.toggle("dark");
+        text_container5p.classList.toggle("dark");
+    });
 });
